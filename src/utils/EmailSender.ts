2737952158a9b861973923ad3e49c2314aaf2905
felix <sportelli@gmail.com>
2018-01-13 18:54:02 +0100

    Importation de AWS-SDK plus "typescript"

--- src/utils/EmailSender.ts
+++ src/utils/EmailSender.ts
@@ -1,6 +1,8 @@
+import * as AWS from 'aws-sdk';
+
 class EmailSender {
     public send(data, config) {
-        const AWS = require('aws-sdk');
+        // const AWS = require('aws-sdk');
         const ses = new AWS.SES(
             {
                 accessKeyId: config.aws_ses_access_key,
